db50b40bc325ef1d8ca5fede1d02ac6878be76d6
wislaargolo <wislaargolo1@gmail.com>
2024-03-27 18:52:09 -0300

    implementando UserModel e repository, service, dto e controller correspondentes

---
+++ user/src/main/java/com/ms/user/models/UserModel.java
@@ -0,0 +1,44 @@
+package com.ms.user.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name="TB_USERS")
+public class UserModel implements Serializable {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private String email;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
